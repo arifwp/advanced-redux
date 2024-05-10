faf59a031655395324f0ab424deb7cefa6a11f41
arifwp <arifwahyu696@gmail.com>
2024-05-10 21:13:31 +0700

    add redux

---
+++ src/store/cart-slice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialCartState = {
+  items: [],
+  totalQuantity: 0,
+};
+
+createSlice({
+  name: "cart",
+  initialState: initialCartState,
+  reducers: {
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+      const existingItem = state.items.find((item) => item.id === item.id);
+      if (!existingItem) {
+        state.items.push({
+          itemId: newItem.id,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+          name: newItem.title,
+        });
+      } else {
+        existingItem.quantity = existingItem.quantity + 1;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+      }
+    },
+    removeItemFromCart() {},
+  },
+});
